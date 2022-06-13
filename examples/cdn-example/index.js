cc9a225115bf9aad0132735f68be207238a8f421
cola119 <kohei.ueno119@gmail.com>
2022-06-13 12:02:43 +0900

    feat: Add CDN example app

---
+++ examples/cdn-example/index.js
@@ -0,0 +1,42 @@
+const liff = window.liff;
+const liffMockPlugin = window.liffMock;
+
+liff.use(new liffMockPlugin());
+
+const main = () => {
+  document.getElementById('init_btn').onclick = onInitClick;
+  document.getElementById('login_btn').onclick = login;
+  document.getElementById('get_profile_btn').onclick = getProfile;
+};
+
+const onInitClick = async () => {
+  try {
+    await liff.init({
+      liffId: 'xxx',
+      mock: true,
+    });
+    console.log('liff.init done');
+  } catch (e) {
+    console.error('liff.init error', e);
+  }
+};
+
+const login = async () => {
+  try {
+    await liff.login();
+  } catch (e) {
+    console.log('liff.login done');
+    console.error('liff.login error', e);
+  }
+};
+
+const getProfile = async () => {
+  try {
+    const profile = await liff.getProfile();
+    console.log('liff.getProfile: ', profile);
+  } catch (e) {
+    console.error('liff.getProfile error', e);
+  }
+};
+
+window.onload = main;
